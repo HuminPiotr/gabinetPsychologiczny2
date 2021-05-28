b35eaf661e9cdf07d844b983b07422232b2d42e8
Piotr <humin.piotr@gmail.com>
2021-05-28 13:14:19 +0200

    add google analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -58,6 +58,33 @@ const plugins = [
           ],
         },
       },
+      {
+        resolve: `gatsby-plugin-google-gtag`,
+        options: {
+          // You can add multiple tracking ids and a pageview event will be fired for all of them.
+          trackingIds: [
+            "G-JFDG2MJ4D5", // Google Analytics / GA
+            "G-JFDG2MJ4D5", // Google Ads / Adwords / AW
+            "G-JFDG2MJ4D5", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+          ],
+          // This object gets passed directly to the gtag config command
+          // This config will be shared across all trackingIds
+          gtagConfig: {
+            optimize_id: "OPT_CONTAINER_ID",
+            anonymize_ip: true,
+            cookie_expires: 0,
+          },
+          // This object is used for configuration specific to this plugin
+          pluginConfig: {
+            // Puts tracking script in the head instead of the body
+            head: false,
+            // Setting this parameter is also optional
+            respectDNT: true,
+            // Avoids sending pageview hits from custom paths
+            exclude: ["/preview/**", "/do-not-track/me/too/"],
+          },
+        },
+      },
     
 ]
 
